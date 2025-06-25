b71a8a55b1aeb1dc2f704a754d32fe38b8db3a15
Jay <115220083+Jaymangukiya22@users.noreply.github.com>
2025-06-25 23:45:13 +0530

    june 25

--- sum_of_set_bits.cpp
+++ sum_of_set_bits.cpp
@@ -1,14 +1,28 @@
-#include<iostream>
-#include<vector>
-#include<array>
-#include<map>
-#include<unordered_map>
-#include<cmath>
-#include<list>
-
+#include <iostream>
+#include <cmath>
 using namespace std;
-
 int main(){
-    int n;
-    cin>>n;
+int n;
+cin>>n;//8
+n++;//9
+int length = (int)floor(log2(n));
+int reward,cycle,answer = 0;
+int mod,diff;
+int count = 0;
+for(int index = 0;index<=length;index++){
+    count++;
+    reward = (1<<index);
+    cycle = (1<<(index+1));
+    answer+= ((int)(n/cycle))*reward;//4*1 = 4
+    mod = n%cycle;//9%2=1
+    diff = mod-(cycle/2);
+    if(diff<0){
+        diff = 0;
+    }
+    answer+=diff;
+
+}
+cout<<"answer : "<<answer<<endl;
+cout<<"iterations : "<<count<<endl;
+
 }
